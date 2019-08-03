591881230d0f0c02f09ac359a5a4544ae007fb25
shenry45 <shawn45henry@gmail.com>
2019-08-02 18:53:10 -0700

    actions Get /, Get /:id

--- actionsRouter.js
+++ actionsRouter.js
@@ -1,14 +1,48 @@
 const express = require('express');
+const db = require('./data/helpers/actionModel.js');
 
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-  res.send('getting actions data back!');
+router.get('/', async (req, res) => {
+  try {
+    const actionsInfo = await db.get();
+  
+    res.status(200).json({
+      success: true,
+      action: actionsInfo
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
 })
 
-router.get('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    
+    const validateID = await db.get(id);
 
+    if (id !== null && validateID) {
+      res.status(200).json({
+        success: true,
+        action: validateID
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "ID not found"
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
 })
 
 router.get('/', (req, res) => {
